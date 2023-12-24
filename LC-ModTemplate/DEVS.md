c4069e6eff9200feb908fbc202768ff97f845481
Sakura Kade Hansen <137737608+SakuraKade@users.noreply.github.com>
2023-12-24 14:59:11 +0100

    Initial commit

---
+++ LC-ModTemplate/DEVS.md
@@ -0,0 +1,6 @@
+﻿# Developers
+Please remember to update the Assembly-CSharp.dll as Lethal Company updates.
+It can be found in the following directory:
+```
+C:\Program Files (x86)\Steam\steamapps\common\Lethal Comapny\Lethal Company_Data\Managed
+```
---
+++ LC-ModTemplate/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace LC_ModTemplate
+{
+    public class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "your.guid.here";
+        public const string PLUGIN_NAME = "Your plugin name";
+        public const string PLUGIN_VERSION = "1.0.0"; 
+    }
+}
